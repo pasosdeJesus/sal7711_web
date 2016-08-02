e02fdb99ba6dd9af49c74225384de315b4af70c1
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-08-02 09:03:34 -0500

    actualiza migracion

--- db/migrate/20160802134021_agrega_pais_articulo.rb
+++ db/migrate/20160802134021_agrega_pais_articulo.rb
@@ -1,6 +1,13 @@
 class AgregaPaisArticulo < ActiveRecord::Migration[5.0]
-  def change
+  def up
     add_column :sal7711_gen_articulo, :pais_id, :integer
     add_foreign_key :sal7711_gen_articulo, :sip_pais, column: :pais_id
+    execute <<-SQL
+      UPDATE sal7711_gen_articulo SET pais_id='170';
+    SQL
   end
+  def down
+    remove_foreign_key :sal7711_gen_articulo, :sip_pais
+    remove_column :sal7711_gen_articulo, :pais_id
+  end 
 end
