9fc990dfa3cc3788cb3e030a68052a9aae106343
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-11-22 17:11:50 -0500

    Actualiza

--- app/controllers/sal7711_gen/articulos_controller.rb
+++ app/controllers/sal7711_gen/articulos_controller.rb
@@ -3,7 +3,7 @@
 require 'sal7711_web/concerns/controllers/articulos_controller'
 
 module Sal7711Gen
-  class ArticulosController < ApplicationController
+  class ArticulosController < Sip::ModelosController
  
     include Sal7711Web::Concerns::Controllers::ArticulosController    
 
