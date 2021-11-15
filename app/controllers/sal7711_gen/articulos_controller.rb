3ee1a07d368e22bec7530680dfe95a60cd53e2ba
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-11-14 19:16:54 -0500

    Corrección

--- app/controllers/sal7711_gen/articulos_controller.rb
+++ app/controllers/sal7711_gen/articulos_controller.rb
@@ -3,7 +3,7 @@ require 'sal7711_web/concerns/controllers/articulos_controller'
 module Sal7711Gen
   class ArticulosController < Sip::ModelosController
 
-    load_and_authorize_resource class: Sal7711gen::Articulo
+    load_and_authorize_resource class: Sal7711Gen::Articulo
     include Sal7711Web::Concerns::Controllers::ArticulosController    
 
   end
